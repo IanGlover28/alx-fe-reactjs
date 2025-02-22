45928d3b97c191201362aab929bd91e26cd474dc
Ian Titus Glover <iantitusglover2@gmail.com>
2025-02-22 23:32:21 +0000

    adding other components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import WelcomeMessage from '.components/WelcomeMessage'
+import Header from '.components/Header';
+import MainContent from '.components/MainContent';
+import Footer from './Footer'
 import './App.css'
 
 function App() {
@@ -19,6 +22,9 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
       
     </>
   )
