98027d5a5d973a4799ebd481b90ed12b1630563d
Ian Titus Glover <iantitusglover2@gmail.com>
2025-02-23 13:28:20 +0000

    modifying

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,5 +1,5 @@
 import ProfilePage from "./components/ProfilePage";
-import UserContext from "./UserContext"; // Use correct path
+import UserContext from "./context/UserContext";
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
