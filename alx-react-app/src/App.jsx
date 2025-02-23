297790bd06e042fd7be28abec02632fdca78ef3c
Ian Titus Glover <iantitusglover2@gmail.com>
2025-02-23 00:11:57 +0000

    adding other components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import WelcomeMessage from '.components/WelcomeMessage'
-import Header from '.components/Header';
-import MainContent from '.components/MainContent';
-import Footer from './Footer'
+import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer'
+import UserProfile from './components/UserProfile'
 import './App.css'
 
 function App() {
@@ -12,19 +13,12 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
       <WelcomeMessage />
       <Header />
       <MainContent />
       <Footer />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       
     </>
   )
