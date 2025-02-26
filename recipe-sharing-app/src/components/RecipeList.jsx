917a1ec3aa28e308a667bf80b4d8c5294ae7b0a3
Ian Titus Glover <iantitusglover2@gmail.com>
2025-02-26 19:17:54 +0000

    adding recipe-sharing-app

---
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -0,0 +1,19 @@
+import useRecipeStore from '../store/RecipeStore';
+import '../App.css';
+
+const RecipeList = () => {
+  const recipes = useRecipeStore((state) => state.recipes);
+
+  return (
+    <div className="recipe-list">
+      {recipes.map((recipe) => (
+        <div key={recipe.id} className="recipe-card">
+          <h3>{recipe.title}</h3>
+          <p>{recipe.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default RecipeList;
