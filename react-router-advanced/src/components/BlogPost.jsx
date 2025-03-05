b30755e67da82deb62b5e89f77c524d2d6d020d9
Ian Titus Glover <iantitusglover2@gmail.com>
2025-03-05 14:50:00 +0000

    modify app.jsx

--- react-router-advanced/src/components/BlogPost.jsx
+++ react-router-advanced/src/components/BlogPost.jsx
@@ -1,10 +1,16 @@
-// BlogPost.jsx
-import React from 'react';
-import { useParams } from 'react-router-dom';
+// src/components/BlogPost.jsx
+import React from "react";
+import { useParams } from "react-router-dom";
 
 function BlogPost() {
-  const { postId } = useParams();
-  return <div>Displaying blog post with ID: {postId}</div>;
+  let { id } = useParams(); // Get the dynamic id from the URL
+
+  return (
+    <div>
+      <h2>Blog Post ID: {id}</h2>
+      <p>This is the content for blog post {id}.</p>
+    </div>
+  );
 }
 
 export default BlogPost;
