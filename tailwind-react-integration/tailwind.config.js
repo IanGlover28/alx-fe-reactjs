4aa83da0a8032609b51f39ade0baceb6029cfee3
Ian Titus Glover <iantitusglover2@gmail.com>
2025-03-12 20:48:47 +0000

    modifying tailwind.config

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'media',
+  purge: [],
+  variants: {
+    extend: {
+      padding: ['hover'],
+    }
+  },
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
